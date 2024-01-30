d2c84f8cc0a2aba8de521703a563e1c8107b0395
Evoider <everfrithza@gmail.com>
2024-01-31 00:16:36 +0100

    added enemies model

---
+++ OutBreak/Source/OutBreak/Private/Zombie.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Zombie.h"
+
+#include "HealthComponent.h"
+
+// Sets default values
+AZombie::AZombie()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	HealthComponent = CreateDefaultSubobject<UHealthComponent>(TEXT("HealthComponent"));
+	
+
+	
+}
+
+// Called when the game starts or when spawned
+void AZombie::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AZombie::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
